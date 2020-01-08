ca8ff8fc21fdf722df5db113cb85f87772de3801
UKI-KC-PC-14 <jena12aishu@gmail.com>
2020-01-08 14:05:36 +0530

    first commit

---
+++ min.js
@@ -0,0 +1,105 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {  }); 
+
+SC.stream('/tracks/64297109',function(sound){ }
+
+$('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+
+SC.stream('/tracks/331502283',function(sound){ }
+
+$('#play4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+
+SC.stream('/tracks/557856666',function(sound){ }
+
+$('#play1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+
+SC.stream('/tracks/52340090',function(sound){ }
+
+$('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+
+SC.stream('/tracks/52340090',function(sound){ }
+
+$('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+SC.stream('/tracks/52340090',function(sound){ }
+
+$('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+SC.stream('/tracks/52340090',function(sound){ }
+
+$('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+SC.stream('/tracks/52340090',function(sound){ }
+
+$('#start').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
